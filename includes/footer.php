c516d9ab2799c2d4d223dc1c12da017950fc3198
Asfiya Misba <73386237+asfiya-misba@users.noreply.github.com>
2021-06-26 13:00:41 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,40 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-3">
+          <h6>About Us</h6>
+          <ul>
+
+
+          <li><a href="page.php?type=aboutus">About Us</a></li>
+            <li><a href="page.php?type=privacy">Privacy</a></li>
+          <li><a href="page.php?type=terms">Terms of Use</a></li>
+               <li><a href="admin/">Admin Login</a></li>
+          </ul>
+        </div>
+
+      
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+      <style>.row{
+        width: 100%;
+        text-align: center;
+      }
+      </style>
+        <style>.heart
+        {
+          color:#e25555;
+        }
+        </style>
+      Made with <span class="heart">❤</span> by Asfiya Misba & Bhavana G
+      </div>
+
+    </div>
+  </div>
+</footer>
---
+++ includes/sidebar.php
@@ -0,0 +1,11 @@
+<div class="profile_nav">
+          <ul>
+            <li><a href="profile.php">Profile Settings</a></li>
+              <li><a href="update-password.php">Update Password</a></li>
+            <li><a href="my-booking.php">My Bookings</a></li>
+            <li><a href="post-testimonial.php">Post a Feedback</a></li>
+               <li><a href="my-testimonials.php">My Feedbacks</a></li>
+            <li><a href="logout.php">Sign Out</a></li>
+          </ul>
+        </div>
+      </div>
